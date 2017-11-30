a94c84523f4ab27ae42b3efdda61ced558f237e5
LiaoPan <LiaoPan_2015@163.com>
2017-11-30 20:20:32 +0800

    BinaryTree

--- BinaryTree_py.py
+++ BinaryTree_py.py
@@ -1,5 +1,12 @@
 class Node:
     def __init__(self,data):
-        self.data = data;
-        self.left = None;
-        self.right = None;
+        self.data = data
+        self.left = None
+        self.right = None
+
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+
